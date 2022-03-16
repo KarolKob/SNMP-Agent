53fccbe298f5a5e8c38145fb702d52445249e3bb
KarolKob <42039707+KarolKob@users.noreply.github.com>
2022-03-16 22:03:40 +0100

    Wrong code deletion

--- src/MPASK_CSharp.ClassLib/PDUDecoder.cs
+++ src/MPASK_CSharp.ClassLib/PDUDecoder.cs
@@ -5,30 +5,15 @@ namespace MPASK_CSharp.ClassLib
 {
     public class PDUDecoder
     {
-        public static Dictionary<ValueObject, ValueObject> Decode(byte[] encodedPDU)
+        public static Dictionary<ValueObject, ValueObject> DecodeContent(byte[] encodedPDU)
         {
             Dictionary<ValueObject, ValueObject> decodedValues = new Dictionary<ValueObject, ValueObject>();
 
             int[] allLen = BERDecoder.DecodeLength(encodedPDU);
 
-            return decodedValues;
-        }
+            
 
-        public static RequestID GetRequestID(byte[] encodedPDU)
-        {
-            switch (encodedPDU[0])
-            {
-                case 0xA0:
-                    return RequestID.GetRequest;
-                case 0xA1:
-                    return RequestID.GetNextRequest;
-                case 0xA2:
-                    return RequestID.GetResponse;
-                case 0xA3:
-                    return RequestID.SetRequest;
-                default:
-                    return RequestID.GetRequest;
-            }
+            return decodedValues;
         }
     }
 }
